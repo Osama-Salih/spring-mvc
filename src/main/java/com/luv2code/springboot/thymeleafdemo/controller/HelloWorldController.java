d218741305f3a327483ec289c2b89fd6bbb7a7a1
Osama Salih <osamasalih.ca@gmail.com>
2025-03-13 09:42:26 +0300

    Adding Data to Spring MVC Model

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +10,8 @@ public class HelloWorldController {
 
     @RequestMapping("/show-form")
     public String showForm() {
-        return "helloworld-form";
+//        return "helloworld-form";
+        return "test";
     }
 
     @RequestMapping("/process-form")
@@ -16,4 +19,20 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    @RequestMapping("/process-form-version-two")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+       // read the request parameter from the HTML form
+        String name = request.getParameter("studentName");
+
+       // convert the data to all caps
+        name = name.toUpperCase();
+
+       // create the message
+        String result = "Yo! " + name;
+
+       // add message to the model
+        model.addAttribute("message", result);
+        return "helloworld";
+   }
 }
