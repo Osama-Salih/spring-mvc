fcd9e7a0411f95547f32704f2345e599ed877b55
Osama Salih <osamasalih.ca@gmail.com>
2025-03-17 22:34:37 +0300

    Drop-Down Lists

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,20 +1,30 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+   @Value("${countries}")
+   private List<String> countries;
+
 
    @GetMapping("/show-student-form")
     public String showForm(Model model) {
        Student student = new Student();
 
        model.addAttribute("student", student);
+       model.addAttribute("countries", countries);
+
+
        return "student-form";
    }
 
